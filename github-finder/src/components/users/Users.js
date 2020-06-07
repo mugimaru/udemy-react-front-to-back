8dc08f07d45f91e4b16e3206f7d6f31485092d77
mugimaru73 <Mugimaru@live.ru>
2020-06-07 18:04:24 +0400

    add hardcoded list of users

---
+++ github-finder/src/components/users/Users.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import UserItem from "./UserItem";
+
+class Users extends Component {
+  state = {
+    users: [
+      {
+        id: 2,
+        login: "defunkt",
+        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
+        html_url: "https://github.com/defunkt",
+      },
+      {
+        login: "pjhyett",
+        id: 3,
+        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
+        html_url: "https://github.com/pjhyett",
+      },
+      {
+        login: "wycats",
+        id: 4,
+        avatar_url: "https://avatars0.githubusercontent.com/u/4?v=4",
+        html_url: "https://github.com/wycats",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div style={userStyle}>
+        {this.state.users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+}
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem",
+};
+
+export default Users;
