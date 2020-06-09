07ff3945a180f5abb4e5c71dc1c78a1b3b93df89
mugimaru73 <Mugimaru@live.ru>
2020-06-10 01:07:54 +0400

    cleanup frontend

---
+++ contact-keeper/client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p className="my-1">
+        This is a full stack React app for keeping contacts
+      </p>
+      <p className="bg-dark p">
+        <strong> Version: 1.0.0 </strong>
+      </p>
+    </Fragment>
+  );
+};
+
+export default About;
