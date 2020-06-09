075f128758f3bc5b6ee3ce19c95cf0e9c0ec1440
mugimaru73 <Mugimaru@live.ru>
2020-06-10 00:23:10 +0400

    add contact model

--- contact-keeper/routes/contacts.js
+++ contact-keeper/routes/contacts.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+const Contact = require("../models/Contact");
 
 // @route       GET api/contacts
 // @desc        Get all users contacts
 // @access      Private
-router.get("/", (req, res) => {
-  res.send("Get all users contacts");
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server Error" });
+  }
 });
 
 // @route       POST api/contacts
