17ec79cdcc7a2e3abe2601df08ebe2e9c7913025
mugimaru73 <Mugimaru.73@live.ru>
2020-06-12 00:16:33 +0400

    implement registration

--- contact-keeper/client/src/context/auth/authReducer.js
+++ contact-keeper/client/src/context/auth/authReducer.js
@@ -9,4 +9,49 @@ import {
   CLEAR_ERRORS,
 } from "../types";
 
-export default (state, action) => state;
+export default (state, action) => {
+  switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload,
+      };
+    case REGISTER_SUCCESS:
+      localStorage.setItem("token", action.payload.token);
+      return {
+        ...state,
+        token: action.payload.token,
+        isAuthenticated: true,
+        loading: false,
+      };
+
+    case REGISTER_FAIL:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+        error: action.payload,
+      };
+
+    case AUTH_ERROR:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return { ...state, error: null };
+
+    default:
+      return state;
+  }
+};
