95515d159cd6c563df2f89b69fe89b7bb1870c03
mugimaru73 <Mugimaru@live.ru>
2020-06-11 22:48:25 +0400

    bootstrap auth context

--- contact-keeper/client/src/App.js
+++ contact-keeper/client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
@@ -11,17 +12,19 @@ import About from "./components/pages/About";
 const App = () => {
   return (
     <div className="App">
-      <ContactState>
-        <Router>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Router>
-      </ContactState>
+      <AuthState>
+        <ContactState>
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </Router>
+        </ContactState>
+      </AuthState>
     </div>
   );
 };
