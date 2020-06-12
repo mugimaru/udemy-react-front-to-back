38ca4783c6d55703ed79789ba28e468e0b1d04ed
mugimaru73 <Mugimaru@live.ru>
2020-06-12 19:01:18 +0400

    add backend integration

--- contact-keeper/client/src/components/contacts/Contacts.js
+++ contact-keeper/client/src/components/contacts/Contacts.js
@@ -1,21 +1,35 @@
-import React, { useContext, Fragment } from "react";
+import React, { useContext, Fragment, useEffect } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactContext from "../../context/contact/contactContext";
 import ContactItem from "./ContactItem";
+import Spinner from "../layout/Spinner";
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
-  const { filtered, contacts } = contactContext;
+  const { filtered, contacts, getContacts, loading } = contactContext;
+
+  useEffect(() => {
+    getContacts();
+    // eslint-disable-next-line
+  }, []);
+
+  if ((contacts !== null) & (contacts.length === 0) & !loading) {
+    return <h1>Please add a contact</h1>;
+  }
 
   return (
     <Fragment>
-      <TransitionGroup>
-        {(filtered ? filtered : contacts).map((contact) => (
-          <CSSTransition key={contact.id} timeout={500} classNames="item">
-            <ContactItem contact={contact} />
-          </CSSTransition>
-        ))}
-      </TransitionGroup>
+      {(contacts !== null) & !loading ? (
+        <TransitionGroup>
+          {(filtered ? filtered : contacts).map((contact) => (
+            <CSSTransition key={contact._id} timeout={500} classNames="item">
+              <ContactItem contact={contact} />
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      ) : (
+        <Spinner />
+      )}
     </Fragment>
   );
 };
--- contact-keeper/client/src/context/contact/contactReducer.js
+++ contact-keeper/client/src/context/contact/contactReducer.js
@@ -6,19 +6,35 @@ import {
   UPDATE_CONTACT,
   FILTER_CONTACTS,
   CLEAR_FILTER,
+  CONTACT_ERROR,
+  GET_CONTACTS,
+  CLEAR_CONTACTS,
 } from "../types";
 
 export default (state, action) => {
   switch (action.type) {
+    case GET_CONTACTS:
+      return { ...state, contacts: action.payload, loading: false };
+
     case ADD_CONTACT:
-      return { ...state, contacts: [...state.contacts, action.payload] };
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts],
+        loading: false,
+      };
+
     case DELETE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.filter(
-          (contact) => contact.id !== action.payload
+          (contact) => contact._id !== action.payload
         ),
+        loading: false,
       };
+
+    case CLEAR_CONTACTS:
+      return { ...state, contacts: [], loading: false, error: null };
+
     case SET_CURRENT:
       return { ...state, current: action.payload };
 
@@ -29,8 +45,9 @@ export default (state, action) => {
       return {
         ...state,
         contacts: state.contacts.map((contact) =>
-          contact.id === action.payload.id ? action.payload : contact
+          contact._id === action.payload._id ? action.payload : contact
         ),
+        loading: false,
       };
 
     case FILTER_CONTACTS:
@@ -44,6 +61,9 @@ export default (state, action) => {
 
     case CLEAR_FILTER:
       return { ...state, filtered: null };
+
+    case CONTACT_ERROR:
+      return { ...state, error: action.payload, loading: false };
 
     default:
       return state;
